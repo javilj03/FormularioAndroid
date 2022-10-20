5d88fa13442625d9351c48405b651a20254d90d8
Javier <javijm03@gmail.com>
2022-10-20 18:13:31 +0200

    Funciones creadas

--- app/src/main/java/com/example/formularioandroid/MainActivity.java
+++ app/src/main/java/com/example/formularioandroid/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.formularioandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText txtInputNombre, txtInputApellido, txtInputCorreo, txtInputComent;
     Button btnConfirm, btnLimpiar;
-
+    Persona p;
+    List<Persona> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +32,28 @@ public class MainActivity extends AppCompatActivity {
         txtInputComent = findViewById(R.id.txtInputComent);
 
 
+    }
+
+
+    public void leerTexto(){
+        String nombre = txtInputNombre.getText().toString();
+        String apellido = txtInputApellido.getText().toString();
+        String correo = txtInputCorreo.getText().toString();
+        String comentario = txtInputComent.getText().toString();
+
+        p = new Persona(nombre,apellido,correo,comentario);
+        list.add(p);
+
+        txtInputNombre.setText("");
+        txtInputApellido.setText("");
+        txtInputCorreo.setText("");
+        txtInputComent.setText("");
+
+    }
+    public void limpiar(){
+        txtInputNombre.setText("");
+        txtInputApellido.setText("");
+        txtInputCorreo.setText("");
+        txtInputComent.setText("");
     }
 }
---
+++ app/src/main/java/com/example/formularioandroid/Persona.java
@@ -0,0 +1,17 @@
+package com.example.formularioandroid;
+
+public class Persona {
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String comentario;
+
+    public Persona(String n, String a, String c, String coment){
+        nombre = n;
+        apellidos = a;
+        correo = c;
+        comentario = coment;
+    }
+
+
+}
