e30d72f9c8541d9cd232e0e1fd7ddb1cff302716
Javier <javijm03@gmail.com>
2022-10-20 17:52:52 +0200

    Items declarados

--- app/src/main/java/com/example/formularioandroid/MainActivity.java
+++ app/src/main/java/com/example/formularioandroid/MainActivity.java
@@ -3,16 +3,28 @@ package com.example.formularioandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText txtInputNombre, txtInputApellido, txtinputCorreo, txtInputComent;
+    EditText txtInputNombre, txtInputApellido, txtInputCorreo, txtInputComent;
+    Button btnConfirm, btnLimpiar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnConfirm = findViewById(R.id.btnConfirmar);
+        btnLimpiar = findViewById(R.id.btnLimp);
+
+        txtInputNombre = findViewById(R.id.txtInputName);
+        txtInputApellido = findViewById(R.id.txtInputApellidos);
+        txtInputCorreo = findViewById(R.id.txtInputCorreo);
+        txtInputComent = findViewById(R.id.txtInputComent);
+
+
     }
 }
